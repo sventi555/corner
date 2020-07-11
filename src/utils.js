e98a105207eb4685e5d5c480f31dbf5e55149202
Stephen Edwards <34760298+sventi555@users.noreply.github.com>
2020-07-11 11:58:44 -0400

    Update utils.js
    improves documentation in makeHbTemplate util

--- src/utils.js
+++ src/utils.js
@@ -4,11 +4,12 @@ const path = require('path');
 const hb = require('handlebars');
 
 /**
+ * Creates a handlebars template object from the specified file.
  *
  * @param {string} dirname - The directory in which the caller resides.
  * @param {string} relativePathToTemplate - The path to the template relative to the caller file.
  *
- * @return {any} A handlebars template compiled from the file.
+ * @return {any} A compiled handlebars template.
  */
 function makeHbTemplate(dirname, relativePathToTemplate) {
     return hb.compile(fs.readFileSync(path.join(dirname, relativePathToTemplate), 'utf8'));
