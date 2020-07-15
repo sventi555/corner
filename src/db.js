033c5fda13e619d07a78451aca30fcf04da9af27
Stephen Edwards <34760298+sventi555@users.noreply.github.com>
2020-07-14 23:15:13 -0400

    small updates to meta files and removes debug steps

--- src/db.js
+++ src/db.js
@@ -1,12 +1,6 @@
 const {MongoClient} = require('mongodb');
 
-const logger = require('./logger');
-
-const user = process.env.MONGO_USERNAME;
-const pass = process.env.MONGO_PASSWORD;
-const host = process.env.MONGO_HOST ? process.env.MONGO_HOST : 'localhost';
-const port = process.env.MONGO_PORT ? process.env.MONGO_PORT : 27017;
-const url = process.env.MONGO_URL ? process.env.MONGO_URL : `mongodb://${user}:${pass}@${host}:${port}`;
+const url = process.env.MONGO_URL;
 
 /**
  *  Returns a promise resolving to a mongo client, or
@@ -15,7 +9,6 @@ const url = process.env.MONGO_URL ? process.env.MONGO_URL : `mongodb://${user}:$
  * @returns A client which can be used to connect to mongodb.
  */
 async function getClient() {
-    logger.info(url);
     const client = await MongoClient.connect(url);
 
     return client;
