fe51aa084dfb24ab10f1985182905e9245319a50
Stephen Edwards <34760298+sventi555@users.noreply.github.com>
2020-07-15 20:25:18 -0400

    makes timezone display dynamically on client side (#11)

--- src/utils.js
+++ src/utils.js
@@ -4,21 +4,6 @@ const path = require('path');
 const hb = require('handlebars');
 
 /**
- * Converts epoch time to a readable date/time string (ex. July 12, 2020 3:27 PM).
- *
- * @param {int} ms - The timestamp to be converted (in ms).
- *
- * @returns {string} A date/time string.
- */
-function goodTime(ms) {
-    const date = new Date(ms);
-    const time = date.toLocaleTimeString('en-US', {hour: 'numeric', minute: '2-digit'});
-    const day = date.toLocaleDateString('en-US', {year: 'numeric', month: 'long', day: 'numeric'});
-
-    return `${day} ${time}`;
-}
-
-/**
  * Creates a handlebars template object from the specified file.
  *
  * @param {string} dirname - The directory in which the caller resides.
@@ -43,4 +28,4 @@ function registerPartial(registerAs, dirname, relativePathToPartial) {
     hb.registerPartial(registerAs, fs.readFileSync(path.join(dirname, relativePathToPartial), 'utf8'));
 }
 
-module.exports = {goodTime, makeHbTemplate, registerPartial};
+module.exports = {makeHbTemplate, registerPartial};
