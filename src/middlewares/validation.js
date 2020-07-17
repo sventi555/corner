5f5562d613debb7a48205a4f6fa27963fdbf1234
Stephen Edwards <34760298+sventi555@users.noreply.github.com>
2020-07-16 23:01:18 -0400

    fixes bug where validation died on more than one schema

--- src/middlewares/validation.js
+++ src/middlewares/validation.js
@@ -1,17 +1,22 @@
 function validate(schemas) {
     return (req, res, next) => {
-
         if (schemas.query) {
             const {error} = schemas.query.validate(req.query);
-            return next(error);
+            if (error) {
+                return next(error);
+            }
         }
         if (schemas.body) {
             const {error} = schemas.body.validate(req.body);
-            return next(error);
+            if (error) {
+                return next(error);
+            }
         }
         if (schemas.params) {
             const {error} = schemas.params.validate(req.params);
-            return next(error);
+            if (error) {
+                return next(error);
+            }
         }
 
         return next();
