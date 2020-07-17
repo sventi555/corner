d0bc0b8cdbad88cb02e57b638b1962029eaa68c2
Stephen Edwards <34760298+sventi555@users.noreply.github.com>
2020-07-16 22:27:53 -0400

    multiple improvements to question page(s)

--- src/routes/questions.js
+++ src/routes/questions.js
@@ -9,65 +9,60 @@ const {makeHbTemplate} = require('../utils');
 const {CORNER_PASSWORD} = process.env;
 
 function questionsRoutes(app) {
+    const questionTemplate = makeHbTemplate(__dirname, '../templates/question.hbs');
     const questionsTemplate = makeHbTemplate(__dirname, '../templates/questions.hbs');
-    app.get('/questions', validate({query: joi.object({date: joi.string().pattern(/^[0-1][0-9]-[0-9]{4}$/)})}), async (req, res, next) => {
-        const dbQuery = {'answer': {$ne: null}};
+    app.get('/questions/:id?',
+        validate({
+            query: joi.object({
+                questionContains: joi.string().max(256),
+                answerContains: joi.string().max(256)
+            }),
+            params: joi.object({
+                id: joi.string().length(24)
+            })
+        }), async (req, res, next) => {
+            let client;
+            try {
+                client = await getClient();
+            } catch (err) {
+                return next(err);
+            }
 
-        if (req.query.date) {
-            const month = parseInt(req.query.date.substring(0, 2));
-            const year = parseInt(req.query.date.substring(3));
+            if (req.params.id) {
+                try {
+                    const question = await client.db('corner').collection('questions').findOne({'_id': ObjectID(req.params.id)});
+                    if (!question) {
+                        return next();
+                    }
 
-            const monthStart = new Date(Date.UTC(year, month - 1));
-            const nextMonthStart = new Date(Date.UTC(year, month));
-            dbQuery['timestamp'] = {$gte: monthStart.getTime(), $lt: nextMonthStart.getTime()};
-        }
+                    res.send(questionTemplate({question}));
+                    return next();
+                } catch(err) {
+                    return next(err);
+                } finally {
+                    await client.close();
+                }
+            }
 
-        let client;
-        try {
-            client = await getClient();
-        } catch (err) {
-            return next(err);
-        }
+            const dbQuery = {'answer': {$ne: null}};
+            if (req.query.questionContains) {
+                dbQuery['question'] = {$regex: `.*${req.query.questionContains}.*`};
+            }
+            if (req.query.answerContains) {
+                dbQuery['answer'].$regex = `.*${req.query.answerContains}.*`;
+            }
 
-        try {
-            const questions = await client.db('corner').collection('questions').find(dbQuery).sort('timestamp', -1).limit(100).toArray();
+            try {
+                const questions = await client.db('corner').collection('questions').find(dbQuery).sort('timestamp', -1).limit(100).toArray();
 
-            res.send(questionsTemplate({questions}));
-            return next();
-        } catch (err) {
-            return next(err);
-        } finally {
-            await client.close();
-        }
-    });
-
-    app.get('/api/questions', basicAuth({challenge: true, users:{'admin': CORNER_PASSWORD}}), async (req, res, next) => {
-        const dbQuery = {};
-
-        if (req.query.answered === 'true') {
-            dbQuery['answer'] = {$ne: null};
-        } else if (req.query.answered === 'false') {
-            dbQuery['answer'] = {$eq: null};
-        }
-
-        let client;
-        try {
-            client = await getClient();
-        } catch (err) {
-            return next(err);
-        }
-
-        try {
-            const questions = await client.db('corner').collection('questions').find(dbQuery).sort('timestamp', -1).toArray();
-
-            res.json({questions});
-            return next();
-        } catch (err) {
-            return next(err);
-        } finally {
-            await client.close();
-        }
-    });
+                res.send(questionsTemplate({questions}));
+                return next();
+            } catch (err) {
+                return next(err);
+            } finally {
+                await client.close();
+            }
+        });
 
     app.post('/api/questions', validate({body: joi.object({question: joi.string().max(256).required()}).required()}), async (req, res, next) => {
         const receivedAt = Date.now();
