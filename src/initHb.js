d01c47310a695534c6223c93ba0c1deabb252d7f
Stephen Edwards <34760298+sventi555@users.noreply.github.com>
2020-07-23 10:55:19 -0400

    adds hbs helper for versioning styles and images

--- src/initHb.js
+++ src/initHb.js
@@ -4,7 +4,10 @@ const {registerHelper, registerPartial} = require('./hbUtils');
  * Initializes handlebars with helpers and partials.
  */
 function initHb() {
+    const LAUNCH_TIME = Date.now();
+
     registerHelper('add', (a, b) => a + b);
+    registerHelper('getLaunchTime', () => LAUNCH_TIME);
 
     registerPartial('base', __dirname, './templates/partials/base.hbs');
     registerPartial('default', __dirname, './templates/partials/default.hbs');
